e22fd1ce6a371d40829467ff5d00e6e453bee19c
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-04-27 00:58:21 +0300

    feat: drogon app that uses www path

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
 #include <drogon/drogon.h>
 
 int main() {
-    // Устанавливаем обработчик для корневого пути "/"
-    drogon::app().registerHandler(
-        "/",
-        [](const drogon::HttpRequestPtr &req,
-           std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
-            auto resp = drogon::HttpResponse::newHttpResponse();
-            resp->setBody("Привет, это Drogon!");
-            callback(resp);
-        },
-        {drogon::Get});
-
-    // Запускаем сервер на 127.0.0.1:8080
     drogon::app()
-        .setLogPath("./logs")  // Опционально: логи в папке logs
-        .addListener("127.0.0.1", 8080)
-        .setThreadNum(4)       // Опционально: количество потоков
+        .setLogPath("./logs")  // Логи будут сохраняться в папку ./logs
+        .setLogLevel(trantor::Logger::kWarn)  // Уровень логирования (только предупреждения)
+        .addListener("0.0.0.0", 8080)  // Слушаем на всех интерфейсах, порт 8080
+        .setDocumentRoot("../www")  // Папка со статическими файлами (HTML, CSS, JS)
+        .enableSession(86400)  // Включаем сессии (таймаут 86400 секунд = 1 день)
         .run();
-
-    return 0;
 }
