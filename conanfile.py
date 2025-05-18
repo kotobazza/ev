fe0819f6a200b7d638ef483dd5ffc6ae6d6a18c1
kotobazza <152398719+kotobazza@users.noreply.github.com>
2025-05-18 23:51:08 +0300

    chore: updated build process for drogon and redis

--- conanfile.py
+++ conanfile.py
@@ -8,31 +8,24 @@ class DrogonAppCustom(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "PkgConfigDeps", "MesonToolchain"
 
+    default_options = {
+        "drogon/*:with_ctl": True,
+        "drogon/*:with_redis": True,
+        "drogon/*:with_postgres": True,
+    }
+
     def requirements(self):
         self.requires("drogon/1.9.10")
         self.requires("gtest/1.16.0")
 
-    def configure(self):
-        self.options["drogon"].with_ctl = True
-        self.options["drogon"].with_redis = True
-        self.options["drogon"].with_postgres = True
-
     def generate(self):
-        # Стандартное окружение для зависимостей
         env = VirtualRunEnv(self)
         env.generate()
 
-        
-
-        # Добавляем путь к drogon_ctl в runenv_info
         drogon_pkg = self.dependencies["drogon"]
         drogon_bin_path = os.path.join(self.build_folder, "deploy", "direct_deploy", "drogon", "bin")
         
         if os.path.exists(drogon_bin_path):
-            # Создаём кастомное окружение
-            
             custom_env = Environment()
             custom_env.prepend_path("PATH", drogon_bin_path)
-            
-            # Сохраняем его в отдельный скрипт
             custom_env.vars(self).save_script("activate_drogon_ctl")
